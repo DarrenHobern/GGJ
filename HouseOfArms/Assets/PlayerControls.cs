0fcf81f0473a356329b557f674c2a0f0871d6f2a
kaiacos <kaiacostanza@gmail.com>
2019-01-26 09:47:55 +1300

    Started working on player cube controls

---
+++ HouseOfArms/Assets/PlayerControls.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public float leftBound = 5;
+    public float rightBound = 10;
+    public float speed = 1;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        Debug.Log(horizontal);
+    }
+}
