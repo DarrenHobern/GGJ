71076c8496520c7c1d9263e38f2354ec45d9adde
kaiacos <kaiacostanza@gmail.com>
2019-01-26 09:58:27 +1300

    Added movement controls to player

--- HouseOfArms/Assets/PlayerControls.cs
+++ HouseOfArms/Assets/PlayerControls.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
-    public float leftBound = 5;
-    public float rightBound = 10;
+    public float leftBound = -5;
+    public float rightBound = 5;
     public float speed = 1;
 
     void Start()
@@ -18,5 +18,14 @@ public class PlayerControls : MonoBehaviour
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         Debug.Log(horizontal);
+        transform.Translate(new Vector3(speed*horizontal, 0, 0));
+        if (transform.position.x <= leftBound)
+        {
+            transform.position = new Vector3(leftBound, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x >= rightBound)
+        {
+            transform.position = new Vector3(rightBound, transform.position.y, transform.position.z);
+        }
     }
 }
