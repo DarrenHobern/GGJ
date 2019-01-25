4702a53a3ddbc02e370ffdecb266bcdb2ee68245
Christopher Brown <christopher.chirat.brown@gmail.com>
2019-01-26 11:38:29 +1300

    The ScenerySpawner script is now compatible with any number of scenery objects.
    The ScenerySpawner script now has a public array that stores references to prefabs. This means we can use as many types of scenery objects as we like.

--- HouseOfArms/Assets/Scripts/ScenerySpawning.cs
+++ HouseOfArms/Assets/Scripts/ScenerySpawning.cs
@@ -4,9 +4,8 @@ using UnityEngine;
 
 public class ScenerySpawning : MonoBehaviour
 {
-    // These are the prefabs we will instantiate during runtime
-    public GameObject houseObject;
-    public GameObject treeObject;
+    // This will hold the prefabs we will instantiate during runtime
+    public GameObject[] sceneryPrefabs = new GameObject[2];
 
     public float minTimeBetweenObjects;
     public float maxTimeBetweenObjects;
@@ -24,10 +23,13 @@ public class ScenerySpawning : MonoBehaviour
             float waitTime = Random.Range(minTimeBetweenObjects, maxTimeBetweenObjects);
             yield return new WaitForSeconds(waitTime);
 
+            // Get a random index corresponding to our array of sceneryPrefabs
+            int typeOfScenery = Random.Range(0, sceneryPrefabs.Length);
+
             // By providing this object's transform as the last parameter,
             // we store all spawned objects under this spawner in the hierarchy.
             // This prevents clutter.
-            Instantiate(houseObject, this.transform.position, Quaternion.identity, this.transform);
+            Instantiate(sceneryPrefabs[typeOfScenery], this.transform.position, Quaternion.identity, this.transform);
         }
     }
 }
