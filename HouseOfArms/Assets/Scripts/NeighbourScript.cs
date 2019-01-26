7112aac84d812253fd2d1b4b641bcebf86cb7fba
kaiacos <kaiacostanza@gmail.com>
2019-01-26 15:21:31 +1300

    Make a wall of trash with its own special spawning thing

---
+++ HouseOfArms/Assets/Scripts/NeighbourScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeighbourScript : MonoBehaviour
+{
+    [SerializeField] private Transform SpawnedStuff;
+    [SerializeField] private Transform[] positionList;
+    [SerializeField] private GameObject trashThing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform placeToSpawn = GetRandomChild();
+        Instantiate(trashThing, placeToSpawn.position, placeToSpawn.rotation, SpawnedStuff);
+
+
+    }
+
+    Transform GetRandomChild()
+    {
+        int positionIndex = Random.Range(0, positionList.Length);
+        int subPositionIndex = Random.Range(0, positionList[positionIndex].childCount);
+        return positionList[positionIndex].GetChild(subPositionIndex);
+
+    }
+}
