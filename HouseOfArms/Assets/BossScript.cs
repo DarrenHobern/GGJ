298c37fbb73a579dc629d5cce267f653d7ec8948
darrenhobern <darren.hobern@gmail.com>
2019-01-27 10:07:38 +1300

    Tidy prefabs folder. Create BossScript
    Organised the prefabs folder.
    Created a bullet person prefab.
    Tidied player and person prefabs.
    Working on adding health to bosses.

---
+++ HouseOfArms/Assets/BossScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossScript : MonoBehaviour
+{
+    private GameControlScript gameControl;
+
+    [SerializeField] private int maxHealth = 10;
+    private int health;
+
+    void Start() {
+        gameControl = GameControlScript.instance;
+        health = maxHealth;
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        if (other.gameObject.CompareTag("Bullet")) {
+            health--;
+            if (health <= 0) {
+                Die();
+            }
+        }
+    }
+
+    private void Die() {
+        // TODO disable and reset this.
+        Reset();
+        gameControl.ExitBossMode();
+    }
+
+    public void Reset() {
+        gameObject.SetActive(false);
+        health = maxHealth;
+        // TODO reset postion
+    }
+}
