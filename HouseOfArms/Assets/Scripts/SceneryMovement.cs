e7d223d06cf7007bd9fc4ccf01a528b8d83019b8
Christopher Brown <christopher.chirat.brown@gmail.com>
2019-01-26 18:41:35 +1300

    Optimised the scenery spawning.

---
+++ HouseOfArms/Assets/Scripts/SceneryMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneryMovement : MonoBehaviour
+{
+    // Speed which the object moves towards the player
+    public float MovingSpeed = 5f;
+
+    // The distance behind the camera at which point the object loops back to the start
+    public float ResetDistanceFromCamera = 2.0f;
+
+    // When the object resets, it goes here. It is the Transform of the parent spawner.
+    public Vector3 PositionToResetTo;
+
+    // When the object gets here, it will reset.
+    Vector3 PositionToResetFrom;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PositionToResetFrom = GameObject.FindGameObjectWithTag("MainCamera").transform.position;
+        PositionToResetTo   = transform.parent.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(-transform.forward * MovingSpeed * Time.deltaTime);
+
+        if (PositionToResetFrom.z > ResetDistanceFromCamera + transform.position.z)
+        {
+            transform.position = PositionToResetTo;
+        }
+    }
+}
--- HouseOfArms/Assets/Scripts/ScenerySpawning.cs
+++ HouseOfArms/Assets/Scripts/ScenerySpawning.cs
@@ -13,11 +13,58 @@ public class ScenerySpawning : MonoBehaviour
     // If we want to mirror objects on one side of the road, we set this to true
     public bool isMirrored;
 
+    public float minimumGap;
+    public float maximumGap;
+
     void Start()
     {
-        StartCoroutine (SpawnObjects());
+        //StartCoroutine (SpawnObjects());
+        //Instantiate(sceneryPrefabs[0], this.transform.position, Quaternion.identity, this.transform);
+
+        // Get the z difference from the camera to this transform.
+        // Instantiate objects to fill that distance.
+        // Random gaps?
+        
+        float start_z = transform.position.z;
+        float end_z   = GameObject.FindGameObjectWithTag("MainCamera").transform.position.z;
+
+        // We will instantiate objects all the way from the start to the end.
+        float totalDifference = start_z - end_z;
+
+        float remainingDifference = totalDifference;
+        while (remainingDifference > 0)
+        {
+            Vector3 spawnLocation = new Vector3(transform.position.x, transform.position.y, end_z + remainingDifference);
+
+            Quaternion rotation;
+            if (isMirrored == false)
+            {
+                rotation = Quaternion.identity;
+            }
+            else
+            {
+                rotation = Quaternion.Euler(0f, 180f, 0f);
+            }
+
+            // Get a random index corresponding to our array of sceneryPrefabs
+            int typeOfScenery = Random.Range(0, sceneryPrefabs.Length);
+
+            // By providing this object's transform as the last parameter,
+            // we store all spawned objects under this spawner in the hierarchy.
+            // This prevents clutter.
+            Instantiate(sceneryPrefabs[typeOfScenery], spawnLocation, rotation, this.transform);
+
+
+            // We will reduce the remaining distance by the size of this gap between this and the next building.
+            float gapSize = Random.Range(minimumGap, maximumGap);
+            remainingDifference -= gapSize;
+        }
+        
     }
 
+    /*
+     * THIS IS OLD UNUSED CODE AND IS ONLY HERE FOR REFERENCE
+     */
     IEnumerator SpawnObjects()
     {
         while (true)
