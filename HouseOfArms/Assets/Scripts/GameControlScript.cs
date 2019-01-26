f8e5accf53bef849cc7b8cb4d4496b3db0ab0f50
Fruzum <sallygillbanks@gmail.com>
2019-01-26 18:55:41 +1300

    Made sure game doesn't start paused and the two paused scenes are not active

--- HouseOfArms/Assets/Scripts/GameControlScript.cs
+++ HouseOfArms/Assets/Scripts/GameControlScript.cs
@@ -32,7 +32,9 @@ public class GameControlScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //TogglePauseMenu();
+        Time.timeScale = 1;
+        PauseScreen.SetActive(false);
+        GameOverScreen.SetActive(false);
     }
 
     // Update is called once per frame
