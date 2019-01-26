03190ae281a9cc0499e22234e1d4922443f33a07
kaiacos <kaiacostanza@gmail.com>
2019-01-26 15:56:55 +1300

    Make boss neighbour fight more tame

--- HouseOfArms/Assets/Scripts/NeighbourScript.cs
+++ HouseOfArms/Assets/Scripts/NeighbourScript.cs
@@ -8,20 +8,17 @@ public class NeighbourScript : MonoBehaviour
     [SerializeField] private Transform[] positionList;
     [SerializeField] private GameObject trashThing;
     [SerializeField] private Vector3 throwVelocity;
+    [SerializeField] private float spawnDelay = 1.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        StartCoroutine(SpawnSomething());
     }
 
     // Update is called once per frame
     void Update()
     {
-        Transform placeToSpawn = GetRandomChild();
-        GameObject trashPiece = Instantiate(trashThing, placeToSpawn.position, placeToSpawn.rotation, SpawnedStuff);
-        Rigidbody trashBody = trashPiece.GetComponent<Rigidbody>();
-        trashBody.AddForce(throwVelocity);
 
     }
 
@@ -30,6 +27,20 @@ public class NeighbourScript : MonoBehaviour
         int positionIndex = Random.Range(0, positionList.Length);
         int subPositionIndex = Random.Range(0, positionList[positionIndex].childCount);
         return positionList[positionIndex].GetChild(subPositionIndex);
+    }
+
+    IEnumerator SpawnSomething()
+    {
+        while (true)
+        {
+            Transform placeToSpawn = GetRandomChild();
+            GameObject trashPiece = Instantiate(trashThing, placeToSpawn.position, placeToSpawn.rotation, SpawnedStuff);
+            Rigidbody trashBody = trashPiece.GetComponent<Rigidbody>();
+            // Only add the force if it's spawning from the sides! 
+            trashBody.AddForce(throwVelocity);
+            yield return new WaitForSeconds(spawnDelay);
+        }
 
     }
+
 }
